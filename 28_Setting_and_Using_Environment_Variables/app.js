0bebfdaaa584fb8e6306c6622b47dced116c456d
Harsh-Arya <harsharyaa666@gmail.com>
2024-11-28 17:12:33 +0530

    console environmentVariables

--- 28_Setting_and_Using_Environment_Variables/app.js
+++ 28_Setting_and_Using_Environment_Variables/app.js
@@ -1,9 +1,13 @@
-
+// setting environment Variables directly in child process Instead of Parent process 
+/* To add/create variable that are not in parent process but in child process type Command
+            Variable name = "value " node --inspect app.js
+*/
 const environmentVariables = process.env;
 
 setInterval(()=>{
     // console.log(process);
     console.log(environmentVariables);
+    console.log('environmentVariables');
 },1000)
 
 
