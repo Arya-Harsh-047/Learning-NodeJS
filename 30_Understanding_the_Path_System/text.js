a09ac777338b63684b2329c70e329a8eb8abc306
Harsh-Arya <harsharyaa666@gmail.com>
2024-11-30 15:12:26 +0530

    problem with window pathe system in js

--- 30_Understanding_the_Path_System/text.js
+++ 30_Understanding_the_Path_System/text.js
@@ -1,1 +1,3 @@
-console.log("Understanding path system")
+console.log("Understanding path system");
+const path1 = "D:\Web Dev\Node JS\Section-3 Basics of OS\30_Understanding_the_Path_System\text.js" //Error
+const path = "D:\\Web Dev\\Node JS\\Section-3 Basics of OS\\30_Understanding_the_Path_System\\text.js"
