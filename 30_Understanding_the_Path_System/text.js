d6170cfc9237d2a2c072aaa0002be5ca4437fae4
Harsh-Arya <harsharyaa666@gmail.com>
2024-11-30 15:34:40 +0530

    Path for windo or linux

--- 30_Understanding_the_Path_System/text.js
+++ 30_Understanding_the_Path_System/text.js
@@ -1,3 +1,12 @@
 console.log("Understanding path system");
-const path1 = "D:\Web Dev\Node JS\Section-3 Basics of OS\30_Understanding_the_Path_System\text.js" //Error
-const path = "D:\\Web Dev\\Node JS\\Section-3 Basics of OS\\30_Understanding_the_Path_System\\text.js"
+// const path1 = "D:\Web Dev\Node JS\Section-3 Basics of OS\30_Understanding_the_Path_System\text.js" //Error
+const absolutePath_Win = "D:\\Web Dev\\Node JS\\Section-3 Basics of OS\\30_Understanding_the_Path_System\\text.js" 
+
+const absolutePath_Linux = "/d/Web Dev/Node JS/Section-3 Basics of OS/30_Understanding_the_Path_System/text.js"
+
+
+const relativePath_window = "30_Understanding_the_Path_System\\text.js"
+
+const relativePath_Linux = "30_Understanding_the_Path_System/text.js"
+
+
