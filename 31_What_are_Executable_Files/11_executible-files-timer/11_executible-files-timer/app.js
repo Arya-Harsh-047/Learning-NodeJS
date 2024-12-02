078dcaf95b7834a85ddeab3581be2ca43fb9f837
Harsh-Arya <harsharyaa666@gmail.com>
2024-12-02 23:53:19 +0530

    Binary Executable File Example

--- 31_What_are_Executable_Files/11_executible-files-timer/11_executible-files-timer/app.js
+++
@@ -1,4 +0,0 @@
-let time = 1;
-setInterval(() => {
-  console.log(time++);
-}, 1000);
---
+++ 31_What_are_Executable_Files/11_executible-files-timer/app.js
@@ -0,0 +1,6 @@
+// Binary Executable File Example
+let time = 1;
+console.log("Hello my name is Harsh Arya")
+setInterval(() => {
+  console.log(time++);
+}, 1000);
